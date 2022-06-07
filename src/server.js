5f65d6bd10137210c7e37b33456dfa428b4a74b4
garnich <garnich@tut.by>
2022-06-07 18:59:53 +0300

    f-m: removed console.logs

--- src/server.js
+++ src/server.js
@@ -1,29 +1,39 @@
 import * as readline from 'node:readline';
-import { stdin as input, stdout as output } from 'process';
-import { getUserName } from './utils/utils';
+import { stdin, stdout } from 'process';
+import { getUserName, getHomeDir } from './utils/utils';
+import { EOL } from 'os';
 
 const reader = () => {
     const userName = getUserName(process.argv);
-    const greeting = `Welcome to the File Manager, ${userName}!`;
+    const homeDir = getHomeDir();
+    const greeting = `Welcome to the File Manager, ${userName}! ${EOL}`;
     const goodBye = `Thank you for using File Manager, ${userName}!`
     
-    console.log(greeting); //TODO: replace
+    const rl = readline.createInterface({ input: stdin, output: stdout });
 
-    const rl = readline.createInterface({ input, output });
+    stdout.write(greeting);
+    stdout.write(EOL);
+    stdout.write(`${homeDir} ${EOL}`);
 
     rl.on('line', (input) => {
-        console.log(`Received: ${input}`);
+        if(input.trim() === 'exit') {
+            stdout.write(goodBye);
 
+            process.exit();
+        }
+
+        stdout.write(`Received: ${input} ${EOL}`);
     });
 
     rl.on('exit', () => {
-        console.log(`Thank you for using File Manager, ${userName}!`);
+        stdout.write(goodBye);
     });
 
     process.openStdin().on("keypress", (chunk, key) => {
         if(key && key.name === "c" && key.ctrl) {
-            console.log(goodBye); //TODO: replace
-          process.exit();
+            stdout.write(goodBye);
+
+            process.exit();
         }
       });
 }
